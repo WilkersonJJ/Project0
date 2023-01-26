e46d4e3158bab26ec1c698ee12000ef7ec0f66bd
Kermalicious <95747898+Kermalicious@users.noreply.github.com>
2023-01-26 12:51:06 -0500

    formatting

--- CS3114-Project0/src/prj0/Project0.java
+++ CS3114-Project0/src/prj0/Project0.java
@@ -1,52 +1,50 @@
 package prj0;
+
 import java.util.Scanner;
+
 /**
  * 
  * @author Jasper Wilkerson & Jade Sherer
  *
  */
 public class Project0 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in); 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		int numNodes = sc.nextInt(); 
-		int numEdges = sc.nextInt(); 
-		int numQueries = sc.nextInt();
-		int count = 0;
-		Node[] nodeContainer = new Node[numNodes];
+        int numNodes = sc.nextInt();
+        int numEdges = sc.nextInt();
+        int numQueries = sc.nextInt();
+        int count = 0;
+        Node[] nodeContainer = new Node[numNodes];
 
-		//this for loop creates all the nodes
-		for (int i = 0; i < numNodes; i++)
-		{
-			Node nodey = new Node(count + 1, numEdges);
-			nodeContainer[count] = nodey;
-			count++;
-		}
-		//this loop reads the two numbers and adds appropriate buddies
-		for (int i = 0; i < numEdges; i++)
-		{
-			int one = sc.nextInt();
-			int two = sc.nextInt();
-			Node node = search(nodeContainer, one);
-			node.addNodeBuddy(two);
-			Node nodeTwo = search(nodeContainer, two);
-			nodeTwo.addNodeBuddy(one);
-		}
-		for (int i = 0; i < numQueries; i++)
-		{
+        // this for loop creates all the nodes
+        for (int i = 0; i < numNodes; i++) {
+            Node nodey = new Node(count + 1, numEdges);
+            nodeContainer[count] = nodey;
+            count++;
+        }
+        // this loop reads the two numbers and adds appropriate buddies
+        for (int i = 0; i < numEdges; i++) {
+            int one = sc.nextInt();
+            int two = sc.nextInt();
+            Node node = search(nodeContainer, one);
+            node.addNodeBuddy(two);
+            Node nodeTwo = search(nodeContainer, two);
+            nodeTwo.addNodeBuddy(one);
+        }
+        for (int i = 0; i < numQueries; i++) {
 
-		}
-		sc.close();
-	}
-	private static Node search(Node[] nodeContainer, int nodeNumber)
-	{
-		for (int i = 0; i < nodeContainer.length; i++)
-		{
-			if (nodeNumber == nodeContainer[i].getName())
-			{
-				return nodeContainer[i];
-			}
-		}
-		return null;
-	}
+        }
+        sc.close();
+    }
+
+
+    private static Node search(Node[] nodeContainer, int nodeNumber) {
+        for (int i = 0; i < nodeContainer.length; i++) {
+            if (nodeNumber == nodeContainer[i].getName()) {
+                return nodeContainer[i];
+            }
+        }
+        return null;
+    }
 }
--- CS3114-Project0/src/prj0/node.java
+++ CS3114-Project0/src/prj0/node.java
@@ -4,42 +4,53 @@ public class Node {
     private int identifier;
     private int[] nodeBuddies;
     private int count;
+
     /**
      * this is the node constructor, which takes in two parameters
-     * @param number the number of the node, basically the "name"
-     * @param maxEdges the "M" value, this is required in order to create the array of node buddies
+     * 
+     * @param number
+     *            the number of the node, basically the "name"
+     * @param maxEdges
+     *            the "M" value, this is required in order to create the array
+     *            of node buddies
      */
-    public Node(int number, int maxEdges)
-    {
+    public Node(int number, int maxEdges) {
         identifier = number;
         nodeBuddies = new int[maxEdges];
         count = 0;
     }
+
+
     /**
      * this method adds a nodeBuddy to the array list and increases the count
-     * @param nodeBuddy the identifier, ie. the number of the node the current node is connected to
+     * 
+     * @param nodeBuddy
+     *            the identifier, ie. the number of the node the current node is
+     *            connected to
      */
-    public void addNodeBuddy(int nodeBuddy)
-    {
+    public void addNodeBuddy(int nodeBuddy) {
         nodeBuddies[count] = nodeBuddy;
         count++;
     }
+
+
     /**
      * returns the indentifier, ie. the "name" of this node
+     * 
      * @return the int identifier of this node
      */
-    public int getName()
-    {
+    public int getName() {
         return identifier;
     }
+
+
     /**
      * this will return the array of all node this node is connected to
+     * 
      * @return the list of nodeBuddies
      */
-    public int[] getBuddies()
-    {
-        if (count == 0)
-        {
+    public int[] getBuddies() {
+        if (count == 0) {
             return null;
         }
         return nodeBuddies;
