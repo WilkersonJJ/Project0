238c45a8d4068dad1c076dad6b61e587eb519e65
WilkersonJJ <103959258+WilkersonJJ@users.noreply.github.com>
2023-01-29 16:15:47 -0500

    Updated code 1/29

--- CS3114-Project0/src/prj0/Project0.java
+++ CS3114-Project0/src/prj0/Project0.java
@@ -25,39 +25,43 @@ public class Project0 {
         int numNodes = sc.nextInt();
         int numEdges = sc.nextInt();
         int numQueries = sc.nextInt();
-        int count = 0;
-        Node[] nodeContainer = new Node[numNodes];
 
-        // This for loop creates all the nodes
-        for (int i = 0; i < numNodes; i++) {
-            Node nodey = new Node(count + 1, numEdges);
-            nodeContainer[count] = nodey;
-            count++;
-        }
-        // This loop reads the two numbers and adds appropriate buddies
+        int[][] data = new int[numNodes][numNodes];
+
+        // This loop reads the two numbers and +1 to every pair
         for (int i = 0; i < numEdges; i++) {
             int one = sc.nextInt();
             int two = sc.nextInt();
-            Node node = search(nodeContainer, one);
-            node.addNodeBuddy(two);
-            Node nodeTwo = search(nodeContainer, two);
-            nodeTwo.addNodeBuddy(one);
+            
+            //since the array is indexed at 0 but our data starts at 1, to find the data we search at n - 1
+            data[one - 1][two - 1]++;
+            data[two - 1][one - 1]++;
         }
+        //this loop goes through all the queries and prints for each query
         for (int i = 0; i < numQueries; i++) {
+            StringBuilder str = new StringBuilder();
+            int q = sc.nextInt(); //q = current query
+            //this loop iterates down the column and appends if the data is >0
 
+            //boolean to check if any adjacencies are found
+            boolean found  = false;
+            for (int j = 0; j < numNodes; j++)
+            {
+                //again, all data is stored one back, so subtract and add to accomadate
+                if (data[q - 1][j] > 0 )
+                {
+                    found = true;
+                    int index = j + 1;
+                    str.append(index + " ");
+                }
+            }
+            if (!found)
+            {
+                str.append(-1);
+            }
+            System.out.println(str);
         }
-        
         // Closes the scanner object when we're done with it
         sc.close();
-    }
-
-
-    private static Node search(Node[] nodeContainer, int nodeNumber) {
-        for (int i = 0; i < nodeContainer.length; i++) {
-            if (nodeNumber == nodeContainer[i].getName()) {
-                return nodeContainer[i];
-            }
-        }
-        return null;
     }
 }
